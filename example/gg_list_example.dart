1d12895df9f3b4a595f760dd13e8297cb4ed0c29
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2024-01-20 05:25:07 +0100

    Initial boylerplate

---
+++ example/gg_list_example.dart
@@ -0,0 +1,6 @@
+import 'package:gg_list/gg_list.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/gg_list_test.dart
@@ -0,0 +1,16 @@
+import 'package:gg_list/gg_list.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
